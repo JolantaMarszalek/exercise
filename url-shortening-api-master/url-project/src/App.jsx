2e81d6dfc5c458c5cc63783b5d4c455f5115a1b8
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-05-24 18:40:45 +0200

    Added Routes

--- url-shortening-api-master/url-project/src/App.jsx
+++ url-shortening-api-master/url-project/src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
+import {
+  Routes,
+  // Route
+} from "react-router-dom";
 
 function App() {
-  return;
+  return <Routes>{/* <Route path="/" element={<MainPage />} /> */}</Routes>;
 }
 
 export default App;
