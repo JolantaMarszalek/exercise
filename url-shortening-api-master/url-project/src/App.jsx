03eb52c45f0db4f9524146ae9dd085c8b8dc068e
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-05-25 18:31:52 +0200

    Added changes

--- url-shortening-api-master/url-project/src/App.jsx
+++ url-shortening-api-master/url-project/src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
-import {
-  Routes,
-  // Route
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout.component";
+import { MainPage } from "./pages/MainPage";
 
 function App() {
   return (
     <Layout>
-      <Routes>{/* <Route path="/" element={<MainPage />} /> */}</Routes>;
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+      ;
     </Layout>
   );
 }
