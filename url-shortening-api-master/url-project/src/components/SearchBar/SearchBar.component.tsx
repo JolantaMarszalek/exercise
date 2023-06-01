108947f510e7d90b31681afca58e543fcbbb225d
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-06-01 08:33:58 +0200

    Added function

--- url-shortening-api-master/url-project/src/components/SearchBar/SearchBar.component.tsx
+++ url-shortening-api-master/url-project/src/components/SearchBar/SearchBar.component.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "../Input/Input.component";
 import { ButtonSquare } from "../Buttons/Buttons.styled";
 import { SearchBarLeft, SearchBarSection } from "./SearchBar.styled";
 
 export const SearchBar = () => {
+  const [originalLink, setOriginalLink] = useState("");
+  const [shortLink, setShortLink] = useState("");
+
   return (
     <>
       <SearchBarSection>
