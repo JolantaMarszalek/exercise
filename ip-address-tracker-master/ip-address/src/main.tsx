6d4bb4d744000660493803c6a75fd25b95fdb8f1
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-06-24 21:25:54 +0200

    Update

--- ip-address-tracker-master/ip-address/src/main.tsx
+++ ip-address-tracker-master/ip-address/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { GlobalStyle } from "./theme/globalStyles.ts";
+// import { GlobalStyle } from "./theme/globalStyles.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
