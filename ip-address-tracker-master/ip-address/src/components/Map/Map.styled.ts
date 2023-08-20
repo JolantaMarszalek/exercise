405731ff2658090e80438fda07d32a0f4ea13c24
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-08-20 12:03:35 +0200

    update

--- ip-address-tracker-master/ip-address/src/components/Map/Map.styled.ts
+++ ip-address-tracker-master/ip-address/src/components/Map/Map.styled.ts
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const MapSection = styled.div`
   display: flex;
   flex-direction: column;
-  /* width: 100%; */
   height: 500px;
   background-color: grey;
   position: relative;
--- ip-address-tracker-master/ip-address/src/pages/MainPage.tsx
+++ ip-address-tracker-master/ip-address/src/pages/MainPage.tsx
@@ -1,8 +1,6 @@
 import Header from "../components/Header/Header.component";
 import Map from "../components/Map/Map.component";
 import MiddleData from "../components/MiddleData/MiddleData.component";
-// import { Title } from "../components/Header/Header.styled";
-// import { SearchBar } from "../components/SearchBar/SearchBar.component";
 
 export const MainPage = () => {
   return (
