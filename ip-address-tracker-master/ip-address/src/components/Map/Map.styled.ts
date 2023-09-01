ad5ab6f027a302d349f9546b602c6ab41d17c924
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-09-01 14:53:10 +0200

    update

--- ip-address-tracker-master/ip-address/src/components/Map/Map.styled.ts
+++ ip-address-tracker-master/ip-address/src/components/Map/Map.styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const MapSection = styled.div`
   display: flex;
   flex-direction: column;
-  height: 500px;
+  height: 550px;
   background-color: hsl(330, 8%, 95%);
   position: relative;
   z-index: 1;
