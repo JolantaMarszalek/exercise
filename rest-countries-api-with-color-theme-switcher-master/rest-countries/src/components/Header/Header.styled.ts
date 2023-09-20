8c54f65cbcbd34a9cae158027e9f8af3c5457c91
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-09-20 06:46:42 +0200

    update

--- rest-countries-api-with-color-theme-switcher-master/rest-countries/src/components/Header/Header.styled.ts
+++ rest-countries-api-with-color-theme-switcher-master/rest-countries/src/components/Header/Header.styled.ts
@@ -8,3 +8,19 @@ export const Title = styled.div`
   padding-left: 50px;
   padding-top: 50px;
 `;
+
+export const HeaderSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: var(--background-color-elements);
+  color: var(--color-text);
+`;
+export const HeaderSectionLeft = styled.div`
+  background-color: var(--background-color-elements);
+  color: var(--color-text);
+`;
+export const HeaderSectionRight = styled.div`
+  background-color: var(--background-color-elements);
+  color: var(--color-text);
+`;
