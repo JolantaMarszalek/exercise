5edaf5ab7022bcad86896b78c8ad02278b96f805
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-10-05 17:24:21 +0200

    update

--- advice-generator-app-main/advice-generator/src/components/Button/Button.styled.ts
+++ advice-generator-app-main/advice-generator/src/components/Button/Button.styled.ts
@@ -3,14 +3,14 @@ import styled from "styled-components";
 export const Button = styled.button`
   background-color: var(--primary-neon-green);
   margin: auto;
-  width: 50px;
-  height: 50px;
+  width: 60px;
+  height: 60px;
   border-radius: 50%;
   display: inline-block;
   text-align: center;
   font-size: 15px;
   svg {
-    width: 20px;
-    height: 20px;
+    width: auto;
+    height: auto;
   }
 `;
--- advice-generator-app-main/advice-generator/src/components/Card/Card.styled.ts
+++ advice-generator-app-main/advice-generator/src/components/Card/Card.styled.ts
@@ -21,8 +21,8 @@ export const CardStyled = styled.div`
   flex-direction: column;
   text-align: center;
   justify-content: space-around;
-  width: 350px;
-  height: 250px;
+  width: 650px;
+  height: 450px;
   border-radius: 10px;
   background-color: var(--neutral-grayish-blue);
   color: var(--primary-light-cyan);
@@ -63,6 +63,27 @@ export const QuoteStyled = styled.div`
     height: auto;
   }
 `;
+
+export const DecorationStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  width: auto;
+  height: auto;
+
+  svg {
+    width: auto;
+    height: auto;
+  }
+
+  @media (max-width: 375px) {
+    width: auto;
+    height: auto;
+  }
+`;
+
 export const ButtonStyled = styled.div`
   display: flex;
   flex-direction: column;
