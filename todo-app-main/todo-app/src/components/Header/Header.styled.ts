bc4653f14d9c09e3e26a765fbbbce8503eb366cd
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-10-06 20:05:22 +0200

    update

--- todo-app-main/todo-app/src/components/Header/Header.styled.ts
+++ todo-app-main/todo-app/src/components/Header/Header.styled.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Title = styled.div`
-  background-color: var(--background-color-elements);
-  color: var(--color-text);
+  background-color: var(--dark-theme-very-dark-grayish-blue);
+  color: var(--dark-theme-light-grayish-blue-hover);
   font-size: 35px;
   font-weight: bold;
   padding: 50px;
@@ -12,14 +12,14 @@ export const HeaderSection = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  background-color: var(--background-color-elements);
-  color: var(--color-text);
+  background-color: var(--dark-theme-very-dark-grayish-blue);
+  color: var(--dark-theme-light-grayish-blue-hover);
 `;
 export const HeaderSectionLeft = styled.div`
-  background-color: var(--background-color-elements);
-  color: var(--color-text);
+  background-color: var(--dark-theme-very-dark-grayish-blue);
+  color: var(--dark-theme-light-grayish-blue-hover);
 `;
 export const HeaderSectionRight = styled.div`
-  background-color: var(--background-color-elements);
-  color: var(--color-text);
+  background-color: var(--dark-theme-very-dark-grayish-blue);
+  color: var(--dark-theme-light-grayish-blue-hover);
 `;
