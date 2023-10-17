cc19d2e77d72ce8501d255946e227d605b318e8b
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-10-17 13:20:41 +0200

    Fixed

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,22 +1,62 @@
-// const inputBill = document.querySelector("#input_bill");
-// inputBill.addEventListener("change", (event) => {});
+document.addEventListener("DOMContentLoaded", function () {
+  const inputBill = document.querySelector(".input_bill");
+  const inputNumberOfPeople = document.querySelector(".input_number_of_people");
+  const resultTotal = document.querySelector(".result_total");
+  const resultTipAmount = document.querySelector(".result_tip_amount");
+  const resetButton = document.querySelector(".button_reset");
 
-function calculate() {
-    const inputBill = document.querySelector("input_bill").value;
-    const inputPeople = document.querySelector("nput_number_of_people").value;
-    const results = Number(inputBill) * Number(inputPeople);
-    document.querySelector("result_total").innerHTML = "$ " + results;
+  function calculate() {
+    const billAmount = parseFloat(inputBill.value);
+    const numberOfPeople = parseFloat(inputNumberOfPeople.value);
+
+    const selectedTipPercentage = getSelectedTipPercentage();
+
+    const tipAmount = (billAmount * selectedTipPercentage) / 100;
+    const totalAmount = billAmount + tipAmount;
+    const totalPerPerson = totalAmount / numberOfPeople;
+
+    resultTipAmount.innerText = "$" + tipAmount.toFixed(2);
+    resultTotal.innerText = "$" + totalPerPerson.toFixed(2);
   }
-  calculate();
-  
-  // document.forms.
-  
-  // ---- Button Reset
-  
-  const startPrice = () => {
-    let priceValue = 0;
-  };
-  
-  const resetButton = document.querySelector("#reset");
-  resetButton.addEventListener("click", () => startPrice());
-  
+
+  //   const startPrice = () => {
+  //     let priceValue = 0;
+  //   };
+
+  function getSelectedTipPercentage() {
+    const buttons = document.querySelectorAll(
+      ".container_button_first div, .container_button_second div"
+    );
+    for (const button of buttons) {
+      if (button.classList.contains("selected")) {
+        return parseFloat(button.textContent);
+      }
+    }
+    return 0;
+  }
+
+  resetButton.addEventListener("click", () => {
+    inputBill.value = "";
+    inputNumberOfPeople.value = "";
+    resultTipAmount.innerText = "$0.00";
+    resultTotal.innerText = "$0.00";
+    clearSelectedTipButtons();
+  });
+
+  const tipButtons = document.querySelectorAll(
+    ".container_button_first div, .container_button_second div"
+  );
+  tipButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      clearSelectedTipButtons();
+      button.classList.add("selected");
+      calculate();
+    });
+  });
+
+  function clearSelectedTipButtons() {
+    tipButtons.forEach((button) => {
+      button.classList.remove("selected");
+    });
+  }
+});
