e38309b42edfbac6b2b7d84483b10b4916ee7dcd
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-11-17 14:54:17 +0100

    update

--- easybank-landing-page-master/easybank-landing/src/components/BottomSection/BottomSection.styled.ts
+++ easybank-landing-page-master/easybank-landing/src/components/BottomSection/BottomSection.styled.ts
@@ -145,8 +145,6 @@ export const BottomCardImage = styled.div`
     height: auto;
     border-radius: 5%;
     object-fit: cover;
-    /* width: 100%;
-    height: 100%; */
   }
 
   @media (max-width: 1024px) {
