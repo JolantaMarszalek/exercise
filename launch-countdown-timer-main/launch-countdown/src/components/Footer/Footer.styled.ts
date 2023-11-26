e091229007f08abc20020312e4831a9eace81760
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-11-26 10:30:58 +0100

    UpdateFooterBackground

--- launch-countdown-timer-main/launch-countdown/src/components/Footer/Footer.styled.ts
+++ launch-countdown-timer-main/launch-countdown/src/components/Footer/Footer.styled.ts
@@ -3,23 +3,44 @@ import styled from "styled-components";
 export const FooterSection = styled.div`
   width: 100%;
   height: auto;
-  //background-color: var(--neutral-very-dark-blue);
-  background-image: url("../../../../images/pattern-hills.svg");
-  background-size: cover;
-  background-repeat: no-repeat;
+  background-color: var(--neutral-very-dark-blue);
+  // background-image: url("../../../../images/pattern-hills.svg");
+  // background-size: cover;
+  // background-repeat: no-repeat;
   position: relative;
   justify-content: center;
   align-items: center;
   display: flex;
   flex-direction: column;
+
+  svg {
+    max-width: 100%;
+    height: auto;
+  }
+
+  @media (max-width: 768px) {
+    justify-content: center;
+    align-items: center;
+  }
+
+  @media (max-width: 576px) {
+    justify-content: center;
+    align-items: center;
+  }
+
+  @media (max-width: 375px) {
+    align-items: center;
+    width: 100%;
+  }
 `;
 
 export const FooterSectionImages = styled.div`
-  position: static;
+  //position: static;
   display: flex;
   justify-content: center;
   align-items: center;
-  color: white;
+  background-color: hsl(271, 23%, 18%);
+  width: 100%;
 
   @media (max-width: 768px) {
     justify-content: center;
