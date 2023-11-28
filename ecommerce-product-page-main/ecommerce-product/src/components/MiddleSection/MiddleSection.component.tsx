bd4e66d71e54f854e1b638d61507d791237d4dff
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-11-28 15:55:00 +0100

    update

--- ecommerce-product-page-main/ecommerce-product/src/components/MiddleSection/MiddleSection.component.tsx
+++ ecommerce-product-page-main/ecommerce-product/src/components/MiddleSection/MiddleSection.component.tsx
@@ -1,9 +1,20 @@
-import { MiddleSectionStyle } from "./MiddleSection.styled";
+import {
+  MiddleSectionLeftSide,
+  MiddleSectionStyle,
+} from "./MiddleSection.styled";
+
+import imageProduct1 from "../../../../images/image-product-1-thumbnail.jpg";
 
 export const MiddleSection = () => {
   return (
     <>
-      <MiddleSectionStyle></MiddleSectionStyle>
+      <MiddleSectionStyle>
+        <MiddleSectionLeftSide>
+          {" "}
+          <img src={imageProduct1} />
+          {/* <img src="../../../../images/image-product-1-thumbnail.jpg" /> */}
+        </MiddleSectionLeftSide>
+      </MiddleSectionStyle>
     </>
   );
 };
--- ecommerce-product-page-main/ecommerce-product/src/components/MiddleSection/MiddleSection.styled.ts
+++ ecommerce-product-page-main/ecommerce-product/src/components/MiddleSection/MiddleSection.styled.ts
@@ -4,6 +4,29 @@ export const MiddleSectionStyle = styled.section`
   display: flex;
   justify-content: space-between;
   height: 100px;
+
+  @media (max-width: 1024px) {
+  }
+
+  @media (max-width: 600px) {
+  }
+`;
+
+export const MiddleSectionLeftSide = styled.section`
+  display: flex;
+  justify-content: space-between;
+  height: 100px;
+  position: static;
+  flex-direction: column;
+  display: flex;
+  padding: 15px;
+  justify-content: center;
+
+  img {
+    width: auto;
+    height: auto;
+  }
+
   @media (max-width: 1024px) {
   }
 
