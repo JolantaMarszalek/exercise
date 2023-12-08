f5ffdb1e7ce2234231a2d073f48a8de912b702ac
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-12-08 16:33:28 +0100

    AddedFont

--- multi-step-form-main/multi-step-form/src/theme/globalStyles.ts
+++ multi-step-form-main/multi-step-form/src/theme/globalStyles.ts
@@ -1,7 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap');
+
   body {
+    font-family: 'Ubuntu', sans-serif;
     color: black;
     margin: 0;
   }
