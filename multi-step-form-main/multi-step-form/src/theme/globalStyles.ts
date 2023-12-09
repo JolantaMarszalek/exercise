8cc4b326b6904cc31d624e86865553c716d7e6ff
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-12-09 10:51:49 +0100

    addes styles in globalStyles

--- multi-step-form-main/multi-step-form/src/theme/globalStyles.ts
+++ multi-step-form-main/multi-step-form/src/theme/globalStyles.ts
@@ -8,4 +8,16 @@ export const GlobalStyle = createGlobalStyle`
     color: black;
     margin: 0;
   }
-`;
+
+  #root {
+  --primary-marine-blue: hsl(213, 96%, 18%);
+  --primary-purplish-blue: hsl(243, 100%, 62%);
+  --primary-pastel-blue: hsl(228, 100%, 84%);
+  --primary-light-blue: hsl(206, 94%, 87%);
+  --primary-strawberry-red: hsl(354, 84%, 57%);
+  --neutral-cool-gray: hsl(231, 11%, 63%);
+  --neutral-light-gray: hsl(229, 24%, 87%);
+  --neutral-monolita: hsl(217, 100%, 97%);
+  --neutral-alabaster: hsl(231, 100%, 99%);
+  --neutral-white:  hsl(0, 0%, 100%);
+} `;
