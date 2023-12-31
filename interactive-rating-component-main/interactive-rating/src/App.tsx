b258b177deb4d762386433e916933428e8e5c0f7
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-12-31 14:11:04 +0100

    Created file pages

--- interactive-rating-component-main/interactive-rating/src/App.tsx
+++ interactive-rating-component-main/interactive-rating/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Page1 } from "./pages/Page1.page";
+import { Page2 } from "./pages/Page2.page";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      {" "}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Page1 />} />
+          <Route path="/thankYou" element={<Page2 />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
