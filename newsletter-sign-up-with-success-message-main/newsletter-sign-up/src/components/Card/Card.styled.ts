dd947f93f1cfae6414cbe6b27717dae7b40886a5
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-01-01 10:06:38 +0100

    Created Card Left section

--- newsletter-sign-up-with-success-message-main/newsletter-sign-up/src/components/Card/Card.styled.ts
+++ newsletter-sign-up-with-success-message-main/newsletter-sign-up/src/components/Card/Card.styled.ts
@@ -2,14 +2,55 @@ import styled from "styled-components";
 
 export const CardSection = styled.div`
   display: flex;
+  flex-direction: row;
+  align-content: flex-start;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 900px;
+  height: 650px;
+  background: var(--neutral-white);
+  padding: 2px;
+  border-radius: 20px;
+
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+  }
+
+  @media (max-width: 375px) {
+    padding: 2px;
+  }
+`;
+
+export const CardLeftSection = styled.div`
+  display: flex;
   flex-direction: column;
   align-content: flex-start;
   justify-content: space-evenly;
-  width: 800px;
+  width: 400px;
   height: 600px;
   background: var(--neutral-white);
   padding: 2px;
-  border-radius: 50px;
+
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+  }
+
+  @media (max-width: 375px) {
+    padding: 2px;
+  }
+`;
+
+export const CardRightSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: flex-start;
+  justify-content: space-evenly;
+  width: 400px;
+  height: 600px;
+  background: var(--neutral-white);
+  padding: 2px;
 
   @media (max-width: 1024px) {
     display: flex;
