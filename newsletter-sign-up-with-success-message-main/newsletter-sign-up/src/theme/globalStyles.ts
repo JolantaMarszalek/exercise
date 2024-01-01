97d45db3ff4d94eb9791dc0afcefde641244e8a5
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-01-01 09:37:44 +0100

    Created GlobalStyle and Layout

---
+++ newsletter-sign-up-with-success-message-main/newsletter-sign-up/src/theme/globalStyles.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    margin: 0;
+  }
+
+  #root {
+  --primary-toato: hsl(4, 100%, 67%);
+  --neutral-dark-slate-grey: hsl(234, 29%, 20%);
+  --neutral-charcoal-grey: hsl(235, 18%, 26%);
+  --neutral-grey: hsl(231, 7%, 60%);
+  --neutral-white: hsl(0, 0%, 100%);
+} 
+`;
