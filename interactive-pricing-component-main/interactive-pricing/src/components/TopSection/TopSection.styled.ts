5263bc7a8ac0f9224c58d49262aba724856f2f8b
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-01-18 16:23:31 +0100

    Created files Card, Layout, TopSection

---
+++ interactive-pricing-component-main/interactive-pricing/src/components/TopSection/TopSection.styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const TopSectionStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+
+  @media (max-width: 700px) {
+  }
+
+  @media (max-width: 375px) {
+  }
+`;
