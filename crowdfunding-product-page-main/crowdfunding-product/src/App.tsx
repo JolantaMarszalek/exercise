7b8f531b2b23bc0077cdd5fb34c6560817b38494
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-01-30 12:16:33 +0100

    Added background in Header

--- crowdfunding-product-page-main/crowdfunding-product/src/App.tsx
+++ crowdfunding-product-page-main/crowdfunding-product/src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from "./components/Header/Header.component";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
 }
 
 export default App;
---
+++ crowdfunding-product-page-main/crowdfunding-product/src/components/Header/Header.styled.ts
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+
+export const HeaderSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: auto;
+  width: auto;
+  position: relative;
+  /* z-index: 2; */
+
+  @media (max-width: 1024px) {
+    width: auto;
+  }
+
+  @media (max-width: 600px) {
+  }
+`;
+
+export const HeaderSectionBackground = styled.div`
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+`;
+
+export const HeaderSectionLogo = styled.div`
+  position: static;
+  /* flex-direction: column; */
+  display: flex;
+  padding: 15px;
+  justify-content: center;
+  margin-left: 100px;
+  z-index: 1;
+`;
+
+export const HeaderSectionLink = styled.div`
+  position: static;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  display: flex;
+  gap: 20px;
+  margin: 10px;
+  /* width: auto; */
+  /* color: var(); */
+  z-index: 1;
+`;
+
+export const HeaderSectionSingleLink = styled.div`
+  position: static;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  display: flex;
+  gap: 20px;
+  margin: 10px;
+  width: auto;
+  /* color: var(); */
+`;
+
+export const HeaderSectionRightSide = styled.div`
+  position: static;
+  display: flex;
+  padding: 15px;
+  justify-content: center;
+  margin-right: 100px;
+`;
+
+export const HeaderSectionCart = styled.div`
+  position: static;
+  flex-direction: column;
+  display: flex;
+  padding: 15px;
+  justify-content: center;
+`;
+
+export const HeaderSectionAvatar = styled.div`
+  position: static;
+  flex-direction: column;
+  display: flex;
+  padding: 15px;
+  justify-content: center;
+
+  img {
+    width: 50px;
+    height: 50px;
+  }
+`;
