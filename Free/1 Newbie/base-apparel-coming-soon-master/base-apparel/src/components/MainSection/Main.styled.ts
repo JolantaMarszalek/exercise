07fc9c1f4d7ce7e6b6abb221c7c1721d323178cd
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-02-13 13:44:30 +0100

    Created Input

--- Free/1 Newbie/base-apparel-coming-soon-master/base-apparel/src/components/MainSection/Main.styled.ts
+++ Free/1 Newbie/base-apparel-coming-soon-master/base-apparel/src/components/MainSection/Main.styled.ts
@@ -25,6 +25,7 @@ export const MainSectionLeft = styled.div`
   justify-content: space-evenly;
   width: auto;
   height: 100vh;
+  align-items: center;
 
   .background-svg {
     width: 830px;
@@ -51,18 +52,9 @@ export const MainSectionLeftText = styled.div`
   flex-direction: column;
   align-content: flex-start;
   justify-content: space-evenly;
-  width: 830px;
+  width: 450px;
   height: 100vh;
   position: absolute;
-  /* 
-  .background-svg {
-    width: 830px;
-    height: 800px;
-    background-image: url("../../../../images/bg-pattern-desktop.svg");
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-  } */
 
   @media (max-width: 1024px) {
     display: flex;
@@ -78,9 +70,7 @@ export const MainSectionLeftText = styled.div`
 export const MainSectionLeftLogo = styled.div`
   display: flex;
   flex-direction: column;
-  /* position: absolute; */
   width: auto;
-  /* transform: translate(-50%, -50%); */
 
   @media (max-width: 1024px) {
     display: flex;
@@ -96,8 +86,45 @@ export const MainSectionLeftLogo = styled.div`
 export const MainSectionLeftTitle = styled.div`
   display: flex;
   flex-direction: column;
-  /* position: absolute; */
   width: auto;
+  text-transform: uppercase;
+  font-size: 65px;
+  letter-spacing: 10px;
+
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    height: 275px;
+  }
+
+  @media (max-width: 375px) {
+  }
+`;
+
+export const MainSectionLeftTitleFirst = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: auto;
+  text-transform: uppercase;
+  color: var(--primary-desaturated-red);
+
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    height: 275px;
+  }
+
+  @media (max-width: 375px) {
+  }
+`;
+
+export const MainSectionLeftTitleSecond = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: auto;
+  text-transform: uppercase;
 
   @media (max-width: 1024px) {
     display: flex;
@@ -113,8 +140,8 @@ export const MainSectionLeftTitle = styled.div`
 export const MainSectionLeftDescribe = styled.div`
   display: flex;
   flex-direction: column;
-  /* position: absolute; */
   width: auto;
+  color: var(--primary-desaturated-red);
 
   @media (max-width: 1024px) {
     display: flex;
@@ -129,9 +156,46 @@ export const MainSectionLeftDescribe = styled.div`
 
 export const MainSectionLeftInputSection = styled.div`
   display: flex;
+  /* flex-direction: column; */
+  width: 100%;
+
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+  }
+
+  @media (max-width: 375px) {
+  }
+`;
+
+export const MainSectionLeftInput = styled.input`
+  display: flex;
   flex-direction: column;
-  /* position: absolute; */
-  width: auto;
+  width: 450px;
+  height: 50px;
+  border-radius: 30px;
+  border: 1px solid var(--primary-desaturated-red);
+
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+  }
+
+  @media (max-width: 375px) {
+  }
+`;
+
+export const MainSectionLeftInputButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  width: 100px;
+  height: 55px;
+  border-radius: 30px;
+  margin-left: -50px;
+  border: none;
+  background-image: linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%));
+  justify-content: center;
+  align-items: center;
 
   @media (max-width: 1024px) {
     display: flex;
