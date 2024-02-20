c8ae4a9e01c21591702c6e201da4e1e6ae85f426
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-02-20 12:51:35 +0100

    Changed function in Card

--- Free/1 Newbie/faq-accordion-main/faq-accordion/src/components/Card/Card.component.tsx
+++ Free/1 Newbie/faq-accordion-main/faq-accordion/src/components/Card/Card.component.tsx
@@ -10,34 +10,46 @@ import {
 
 import { useState } from "react";
 
-export const Card = () => {
-  const [clickedQuestionIndex, setClickedQuestionIndex] = useState<
-    number | null
-  >(null);
+interface FAQ {
+  question: string;
+  answer: string;
+  isOpen: boolean;
+}
 
-  const faqData = [
+export const Card = () => {
+  const [faqData, setFaqData] = useState<FAQ[]>([
     {
       question: "What is Frontend Mentor, and how will it help me?",
       answer:
         "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+      isOpen: false,
     },
     {
       question: "Is Frontend Mentor free?",
       answer:
         "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+      isOpen: false,
     },
     {
       question: "Can I use Frontend Mentor projects in my portfolio?",
       answer:
         "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+      isOpen: false,
     },
     {
       question:
         "How can I get help if I'm stuck on a Frontend Mentor challenge?",
       answer:
         "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+      isOpen: false,
     },
-  ];
+  ]);
+
+  const toggleAnswer = (index: number) => {
+    const updatedFaqData = [...faqData];
+    updatedFaqData[index].isOpen = !updatedFaqData[index].isOpen;
+    setFaqData(updatedFaqData);
+  };
 
   return (
     <>
@@ -63,7 +75,7 @@ export const Card = () => {
             <CardSectionRightSingleLineQuestion>
               {faq.question}
               <CardSectionRightSingleLineQuestionImage
-                onClick={() => setClickedQuestionIndex(index)}>
+                onClick={() => toggleAnswer(index)}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="30"
@@ -77,7 +89,7 @@ export const Card = () => {
                 </svg>
               </CardSectionRightSingleLineQuestionImage>
             </CardSectionRightSingleLineQuestion>
-            {clickedQuestionIndex === index && (
+            {faq.isOpen && (
               <CardSectionRightSingleLineAnswer>
                 {faq.answer}
               </CardSectionRightSingleLineAnswer>
