459561e0df6496322a3f01577db311b4728dd025
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-02-28 16:45:38 +0100

    Created Files - Top, Middle, Bottom

---
+++ Free/2 Junior/blogr-landing-page-main/blogr-landing/src/components/TopSection/TopSection.styled.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const TopSectionStyled = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  position: relative;
+
+  @media (max-width: 1024px) {
+    width: auto;
+  }
+
+  @media (max-width: 600px) {
+  }
+`;
+
+export const TopSectionBackground = styled.div`
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: -1;
+`;
