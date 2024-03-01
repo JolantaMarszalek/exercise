c4b6cdf0f7dd6e25224fcd77a9177f0548e1ea1e
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-03-01 14:13:41 +0100

    Update

--- Free/2 Junior/blogr-landing-page-main/blogr-landing/src/components/Header/Header.styled.ts
+++ Free/2 Junior/blogr-landing-page-main/blogr-landing/src/components/Header/Header.styled.ts
@@ -1,6 +1,37 @@
 import styled from "styled-components";
 
+export const HeaderSectionBackground = styled.section`
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 200px;
+  z-index: -1;
+  /* background-color: red; */
+`;
+
 export const HeaderSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: auto;
+  position: relative;
+  background-color: red;
+
+  @media (max-width: 1024px) {
+    width: auto;
+  }
+
+  @media (max-width: 600px) {
+  }
+`;
+
+export const HeaderSectionTop = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -17,41 +48,29 @@ export const HeaderSection = styled.section`
   }
 `;
 
-export const HeaderSectionBackground = styled.div`
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  z-index: -1;
-  background-color: red;
-`;
-
-export const HeaderSectionLogo = styled.div`
+export const HeaderSectionTopLogo = styled.div`
   margin-left: 50px;
 `;
 
-export const HeaderSectionMenu = styled.div`
+export const HeaderSectionTopMenu = styled.div`
   display: flex;
   gap: 20px;
 `;
 
-export const HeaderSectionMenuSingle = styled.div`
+export const HeaderSectionTopMenuSingle = styled.div`
   color: var(--neutral-white--text);
 `;
 
-export const HeaderSectionLogin = styled.div`
+export const HeaderSectionTopLogin = styled.div`
   display: flex;
   gap: 20px;
   margin-right: 50px;
   align-items: center;
 `;
-export const HeaderSectionLoginSingle = styled.div`
+export const HeaderSectionTopLoginSingle = styled.div`
   color: var(--neutral-white--text);
 `;
-export const HeaderSectionLoginButton = styled.button`
+export const HeaderSectionTopLoginButton = styled.button`
   height: 40px;
   width: 120px;
   background-color: var(--neutral-white--text);
