c44f6246b4c9647a6f25860e6d5b665e2103a0f2
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-03-13 13:11:02 +0100

    Update Footer - First and Second Section

--- Free/2 Junior/fylo-dark-theme-landing-page-master/fylo-dark/src/components/Footer/Footer.styled.ts
+++ Free/2 Junior/fylo-dark-theme-landing-page-master/fylo-dark/src/components/Footer/Footer.styled.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const FooterSection = styled.section`
   display: flex;
+  flex-direction: column;
   background-color: var(--primary-dark-blue--testimonials-background);
   justify-content: space-between;
   align-items: center;
@@ -17,14 +18,88 @@ export const FooterSectionLogo = styled.div`
   flex-direction: column;
   position: static;
   margin: 50px;
+  width: 95%;
 `;
 
-export const FooterSectionText = styled.div`
+export const FooterSectionBottomText = styled.div`
   display: flex;
   position: static;
   color: var(--neutral-white);
   gap: 20px;
   margin: 50px;
+`;
+
+export const FooterSectionBottomTextSectionFirst = styled.div`
+  display: flex;
+  position: static;
+  color: var(--neutral-white);
+  gap: 20px;
+  /* margin: 50px; */
+`;
+
+export const FooterSectionBottomTextSectionFirstImage = styled.div`
+  display: flex;
+  position: static;
+  color: var(--neutral-white);
+  gap: 20px;
+  /* margin: 50px; */
+`;
+
+export const FooterSectionBottomTextSectionFirstText = styled.div`
+  display: flex;
+  position: static;
+  color: var(--neutral-white);
+  gap: 20px;
+  /* margin: 50px; */
+`;
+
+export const FooterSectionBottomTextSectionSecond = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: static;
+  color: var(--neutral-white);
+  gap: 20px;
+  /* margin: 50px; */
+`;
+export const FooterSectionBottomTextSectionSecondSingleLine = styled.div`
+  display: flex;
+
+  position: static;
+  color: var(--neutral-white);
+  gap: 20px;
+  /* margin: 50px; */
+`;
+
+export const FooterSectionBottomTextSectionSecondSingleLineImage = styled.div`
+  display: flex;
+  position: static;
+  color: var(--neutral-white);
+  gap: 20px;
+  /* margin: 50px; */
+`;
+
+export const FooterSectionBottomTextSectionSecondSingleLineText = styled.div`
+  display: flex;
+  position: static;
+  color: var(--neutral-white);
+  gap: 20px;
+  /* margin: 50px; */
+`;
+
+export const FooterSectionBottomTextSectionThird = styled.div`
+  display: flex;
+  position: static;
+  color: var(--neutral-white);
+  gap: 20px;
+  /* margin: 50px; */
+`;
+
+export const FooterSectionBottomTextSectionFourth = styled.div`
+  display: flex;
+  position: static;
+  color: var(--neutral-white);
+  gap: 20px;
+  /* margin: 50px; */
 `;
 
 export const FooterSectionTextSingle = styled.div`
