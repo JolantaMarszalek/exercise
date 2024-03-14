e6dc1c12c82a98f08d08f21b3103e5106d0cd2b4
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-03-14 16:24:54 +0100

    Update styles in Header

--- Free/2 Junior/insure-landing-page-master/insure-landing/src/components/Header/Header.styled.ts
+++ Free/2 Junior/insure-landing-page-master/insure-landing/src/components/Header/Header.styled.ts
@@ -4,6 +4,7 @@ export const HeaderSection = styled.section`
   display: flex;
   /* flex-direction: column; */
   justify-content: space-between;
+  padding: 20px;
 
   @media (max-width: 1024px) {
   }
@@ -12,6 +13,7 @@ export const HeaderSection = styled.section`
 export const HeaderSectionLogo = styled.div`
   display: flex;
   flex-direction: column;
+  padding-left: 100px;
 
   @media (max-width: 1024px) {
   }
@@ -20,6 +22,10 @@ export const HeaderSectionLogo = styled.div`
 export const HeaderSectionAbout = styled.div`
   display: flex;
   /* flex-direction: column; */
+  align-items: center;
+  padding-right: 100px;
+  font-family: "Karla", sans-serif;
+  letter-spacing: 1px;
 
   @media (max-width: 1024px) {
   }
@@ -28,6 +34,10 @@ export const HeaderSectionAbout = styled.div`
 export const HeaderSectionAboutSingle = styled.div`
   display: flex;
   flex-direction: column;
+  text-transform: uppercase;
+  padding: 20px;
+  color: var(--neutral-dark-grayish-violet);
+  font-weight: 700;
 
   @media (max-width: 1024px) {
   }
@@ -36,6 +46,15 @@ export const HeaderSectionAboutSingle = styled.div`
 export const HeaderSectionAboutButton = styled.button`
   display: flex;
   flex-direction: column;
+  text-transform: uppercase;
+  height: 40px;
+  width: 120px;
+  justify-content: center;
+  background-color: white;
+  align-items: center;
+  color: var(--primary-dark-violet);
+  font-weight: 700;
+  letter-spacing: 1px;
 
   @media (max-width: 1024px) {
   }
