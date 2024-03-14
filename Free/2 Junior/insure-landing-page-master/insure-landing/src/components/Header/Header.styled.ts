5cc7199dadaf1a90c40fa7a368750f2494b81175
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-03-14 15:56:23 +0100

    Draft Footer

--- Free/2 Junior/insure-landing-page-master/insure-landing/src/components/Header/Header.styled.ts
+++ Free/2 Junior/insure-landing-page-master/insure-landing/src/components/Header/Header.styled.ts
@@ -2,13 +2,38 @@ import styled from "styled-components";
 
 export const HeaderSection = styled.section`
   display: flex;
+  /* flex-direction: column; */
+  justify-content: space-between;
+
+  @media (max-width: 1024px) {
+  }
+`;
+
+export const HeaderSectionLogo = styled.div`
+  display: flex;
   flex-direction: column;
 
   @media (max-width: 1024px) {
   }
 `;
 
-export const HeaderSectionCard = styled.div`
+export const HeaderSectionAbout = styled.div`
+  display: flex;
+  /* flex-direction: column; */
+
+  @media (max-width: 1024px) {
+  }
+`;
+
+export const HeaderSectionAboutSingle = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 1024px) {
+  }
+`;
+
+export const HeaderSectionAboutButton = styled.button`
   display: flex;
   flex-direction: column;
 
