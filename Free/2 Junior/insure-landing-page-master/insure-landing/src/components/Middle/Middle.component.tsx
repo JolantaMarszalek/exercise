ac1a46e74629682113cddfacbd580d4d82436f13
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-03-15 08:04:01 +0100

    Draft Miidle

--- Free/2 Junior/insure-landing-page-master/insure-landing/src/components/Middle/Middle.component.tsx
+++ Free/2 Junior/insure-landing-page-master/insure-landing/src/components/Middle/Middle.component.tsx
@@ -1,9 +1,49 @@
-import { MiddleSection, MiddleSectionCard } from "./Middle.styled";
+import {
+  MiddleSection,
+  MiddleSectionBottom,
+  MiddleSectionBottomSingle,
+  MiddleSectionBottomSingleDescribe,
+  MiddleSectionBottomSingleImage,
+  MiddleSectionBottomSingleTitle,
+  MiddleSectionTitle,
+} from "./Middle.styled";
 
 export const Middle = () => {
   return (
     <MiddleSection>
-      <MiddleSectionCard></MiddleSectionCard>
+      <MiddleSectionTitle>We’re different</MiddleSectionTitle>
+      <MiddleSectionBottom>
+        <MiddleSectionBottomSingle>
+          <MiddleSectionBottomSingleImage></MiddleSectionBottomSingleImage>
+          <MiddleSectionBottomSingleTitle>
+            Snappy Process
+          </MiddleSectionBottomSingleTitle>
+          <MiddleSectionBottomSingleDescribe>
+            Our application process can be completed in minutes, not hours.
+            Don’t get stuck filling in tedious forms.
+          </MiddleSectionBottomSingleDescribe>
+        </MiddleSectionBottomSingle>
+        <MiddleSectionBottomSingle>
+          <MiddleSectionBottomSingleImage></MiddleSectionBottomSingleImage>
+          <MiddleSectionBottomSingleTitle>
+            Affordable Prices
+          </MiddleSectionBottomSingleTitle>
+          <MiddleSectionBottomSingleDescribe>
+            We don’t want you worrying about high monthly costs. Our prices may
+            be low, but we still offer the best coverage possible.
+          </MiddleSectionBottomSingleDescribe>
+        </MiddleSectionBottomSingle>
+        <MiddleSectionBottomSingle>
+          <MiddleSectionBottomSingleImage></MiddleSectionBottomSingleImage>
+          <MiddleSectionBottomSingleTitle>
+            People First
+          </MiddleSectionBottomSingleTitle>
+          <MiddleSectionBottomSingleDescribe>
+            Our plans aren’t full of conditions and clauses to prevent payouts.
+            We make sure you’re covered when you need it.
+          </MiddleSectionBottomSingleDescribe>
+        </MiddleSectionBottomSingle>
+      </MiddleSectionBottom>
     </MiddleSection>
   );
 };
--- Free/2 Junior/insure-landing-page-master/insure-landing/src/components/Middle/Middle.styled.ts
+++ Free/2 Junior/insure-landing-page-master/insure-landing/src/components/Middle/Middle.styled.ts
@@ -8,7 +8,47 @@ export const MiddleSection = styled.section`
   }
 `;
 
-export const MiddleSectionCard = styled.div`
+export const MiddleSectionTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 1024px) {
+  }
+`;
+
+export const MiddleSectionBottom = styled.div`
+  display: flex;
+  /* flex-direction: column; */
+
+  @media (max-width: 1024px) {
+  }
+`;
+
+export const MiddleSectionBottomSingle = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 1024px) {
+  }
+`;
+
+export const MiddleSectionBottomSingleImage = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 1024px) {
+  }
+`;
+
+export const MiddleSectionBottomSingleTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 1024px) {
+  }
+`;
+
+export const MiddleSectionBottomSingleDescribe = styled.div`
   display: flex;
   flex-direction: column;
 
