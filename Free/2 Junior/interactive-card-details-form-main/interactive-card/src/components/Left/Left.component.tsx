b34d92d5b8751bc58a882c2babeed016698148a3
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-03-21 11:39:46 +0100

    Update Left

--- Free/2 Junior/interactive-card-details-form-main/interactive-card/src/components/Left/Left.component.tsx
+++ Free/2 Junior/interactive-card-details-form-main/interactive-card/src/components/Left/Left.component.tsx
@@ -1,9 +1,26 @@
-import { LeftSection, LeftSectionCard } from "./Left.styled";
+import {
+  LeftSection,
+  LeftSectionCard,
+  LeftSectionCardBottom,
+  LeftSectionCardTop,
+} from "./Left.styled";
+
+import imageCardFront from "./../../../../images/bg-card-front.png";
+import imageCardBack from "./../../../../images/bg-card-back.png";
 
 export const Left = () => {
   return (
     <LeftSection>
-      <LeftSectionCard></LeftSectionCard>
+      <LeftSectionCard>
+        <LeftSectionCardTop>
+          <img src={imageCardFront} alt="Card front" />
+        </LeftSectionCardTop>
+        <LeftSectionCardBottom>
+          <LeftSectionCardTop>
+            <img src={imageCardBack} alt="Card back" />
+          </LeftSectionCardTop>
+        </LeftSectionCardBottom>
+      </LeftSectionCard>
     </LeftSection>
   );
 };
--- Free/2 Junior/interactive-card-details-form-main/interactive-card/src/components/Left/Left.styled.ts
+++ Free/2 Junior/interactive-card-details-form-main/interactive-card/src/components/Left/Left.styled.ts
@@ -12,6 +12,25 @@ export const LeftSection = styled.section`
 
 export const LeftSectionCard = styled.div`
   display: flex;
+  flex-direction: column;
+
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const LeftSectionCardTop = styled.div`
+  display: flex;
+
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const LeftSectionCardBottom = styled.div`
+  display: flex;
 
   @media (max-width: 1024px) {
     display: flex;
