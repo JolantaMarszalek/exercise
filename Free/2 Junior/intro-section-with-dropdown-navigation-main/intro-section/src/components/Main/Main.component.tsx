74a5fd74c975eebb44f2738b6bb9d801c56e1409
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-04-04 09:15:06 +0200

    Main draft

---
+++ Free/2 Junior/intro-section-with-dropdown-navigation-main/intro-section/src/components/Main/Main.component.tsx
@@ -0,0 +1,11 @@
+import { MainSection, MainSectionCard } from "./Main.styled";
+
+export const Main = () => {
+  return (
+    <>
+      <MainSection>
+        <MainSectionCard></MainSectionCard>
+      </MainSection>
+    </>
+  );
+};
---
+++ Free/2 Junior/intro-section-with-dropdown-navigation-main/intro-section/src/components/Main/Main.styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const MainSection = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const MainSectionCard = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
