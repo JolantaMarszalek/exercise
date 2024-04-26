59cdf0ad57769f08a510d037664c6ae5f314ac97
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-04-26 06:16:13 +0200

    Changes Menu and Close

--- Free/2 Junior/intro-section-with-dropdown-navigation-main/intro-section/src/components/Header/Header.component.tsx
+++ Free/2 Junior/intro-section-with-dropdown-navigation-main/intro-section/src/components/Header/Header.component.tsx
@@ -57,7 +57,7 @@ export const Header = () => {
   };
 
   const toggleMenuOption = (option: keyof typeof menuOptions) => {
-    console.log("Toggle menu option: ", option);
+    // console.log("Toggle menu option: ", option);
     setMenuOptions((prevOptions) => ({
       ...prevOptions,
       [option]: !prevOptions[option],
@@ -77,9 +77,28 @@ export const Header = () => {
         </HeaderSectionLogo>
         {isMobile && windowWidth <= 1024 && (
           <HeaderSectionMenuIcon className="icon-menu" onClick={toggleMenu}>
-            <svg width="32" height="18" xmlns="http://www.w3.org/2000/svg">
+            {menuOpen ? (
+              <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
+                <g fill="#151515" fillRule="evenodd">
+                  <path d="m2.393.98 22.628 22.628-1.414 1.414L.979 2.395z" />
+                  <path d="M.98 23.607 23.609.979l1.414 1.414L2.395 25.021z" />
+                </g>
+              </svg>
+            ) : (
+              <svg width="32" height="18" xmlns="http://www.w3.org/2000/svg">
+                <g fill="#151515" fillRule="evenodd">
+                  <path d="M0 0h32v2H0zM0 8h32v2H0zM0 16h32v2H0z" />
+                </g>
+              </svg>
+            )}
+          </HeaderSectionMenuIcon>
+        )}
+        {!isMobile && menuOpen && (
+          <HeaderSectionMenuIcon className="icon-menu" onClick={toggleMenu}>
+            <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
               <g fill="#151515" fillRule="evenodd">
-                <path d="M0 0h32v2H0zM0 8h32v2H0zM0 16h32v2H0z" />
+                <path d="m2.393.98 22.628 22.628-1.414 1.414L.979 2.395z" />
+                <path d="M.98 23.607 23.609.979l1.414 1.414L2.395 25.021z" />
               </g>
             </svg>
           </HeaderSectionMenuIcon>
