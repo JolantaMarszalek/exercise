390e11f3e59bea57aa1ad2224d18423d776e6a3a
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-04-26 05:55:07 +0200

    Changes Arrows Up and Down

--- Free/2 Junior/intro-section-with-dropdown-navigation-main/intro-section/src/components/Header/Header.component.tsx
+++ Free/2 Junior/intro-section-with-dropdown-navigation-main/intro-section/src/components/Header/Header.component.tsx
@@ -94,12 +94,21 @@ export const Header = () => {
                 </HeaderSectionMenuSingleFirstLine>
                 <HeaderSectionMenuSingleImage>
                   <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      stroke="#686868"
-                      stroke-width="1.5"
-                      fill="none"
-                      d="m1 1 4 4 4-4"
-                    />
+                    {menuOptions.features ? (
+                      <path
+                        stroke="#686868"
+                        strokeWidth="1.5"
+                        fill="none"
+                        d="M1 5 5 1 9 5"
+                      />
+                    ) : (
+                      <path
+                        stroke="#686868"
+                        strokeWidth="1.5"
+                        fill="none"
+                        d="M1 1 5 5 9 1"
+                      />
+                    )}
                   </svg>
                 </HeaderSectionMenuSingleImage>
                 {menuOptions.features && (
@@ -179,12 +188,21 @@ export const Header = () => {
                 </HeaderSectionMenuSingleFirstLine>
                 <HeaderSectionMenuSingleImage>
                   <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      stroke="#686868"
-                      stroke-width="1.5"
-                      fill="none"
-                      d="m1 1 4 4 4-4"
-                    />
+                    {menuOptions.company ? (
+                      <path
+                        stroke="#686868"
+                        strokeWidth="1.5"
+                        fill="none"
+                        d="M1 5 5 1 9 5"
+                      />
+                    ) : (
+                      <path
+                        stroke="#686868"
+                        strokeWidth="1.5"
+                        fill="none"
+                        d="M1 1 5 5 9 1"
+                      />
+                    )}
                   </svg>
                 </HeaderSectionMenuSingleImage>
                 {menuOptions.company && (
@@ -239,12 +257,21 @@ export const Header = () => {
                 </HeaderSectionMenuSingleFirstLineMobile>
                 <HeaderSectionMenuSingleImageMobile>
                   <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      stroke="#686868"
-                      stroke-width="1.5"
-                      fill="none"
-                      d="m1 1 4 4 4-4"
-                    />
+                    {menuOptions.features ? (
+                      <path
+                        stroke="#686868"
+                        strokeWidth="1.5"
+                        fill="none"
+                        d="M1 5 5 1 9 5"
+                      />
+                    ) : (
+                      <path
+                        stroke="#686868"
+                        strokeWidth="1.5"
+                        fill="none"
+                        d="M1 1 5 5 9 1"
+                      />
+                    )}
                   </svg>
                 </HeaderSectionMenuSingleImageMobile>
               </HeaderSectionMenuSingleMobileFirstLine>
@@ -325,12 +352,21 @@ export const Header = () => {
                 </HeaderSectionMenuSingleFirstLineMobile>
                 <HeaderSectionMenuSingleImageMobile>
                   <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      stroke="#686868"
-                      stroke-width="1.5"
-                      fill="none"
-                      d="m1 1 4 4 4-4"
-                    />
+                    {menuOptions.company ? (
+                      <path
+                        stroke="#686868"
+                        strokeWidth="1.5"
+                        fill="none"
+                        d="M1 5 5 1 9 5"
+                      />
+                    ) : (
+                      <path
+                        stroke="#686868"
+                        strokeWidth="1.5"
+                        fill="none"
+                        d="M1 1 5 5 9 1"
+                      />
+                    )}
                   </svg>
                 </HeaderSectionMenuSingleImageMobile>
               </HeaderSectionMenuSingleMobileFirstLine>
