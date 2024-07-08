eac0e8481e1fa17b71746aeac54516c3fef597df
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-07-08 05:39:54 +0200

    Up

--- Free/2 Junior/contact-form-main/contact-form/src/components/Layout/Layout.styled.ts
+++ Free/2 Junior/contact-form-main/contact-form/src/components/Layout/Layout.styled.ts
@@ -2,5 +2,6 @@ import styled from "styled-components";
 
 export const LayoutStyled = styled.section`
   width: 100%;
-  height: 100%;
+  height: 100vh;
+  background-color: var(--primary-green-200-lighter);
 `;
--- Free/2 Junior/contact-form-main/contact-form/src/components/Main/Main.styled.ts
+++ Free/2 Junior/contact-form-main/contact-form/src/components/Main/Main.styled.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const MainStyled = styled.section`
+  display: flex;
   width: 100%;
   height: 100%;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
 `;
