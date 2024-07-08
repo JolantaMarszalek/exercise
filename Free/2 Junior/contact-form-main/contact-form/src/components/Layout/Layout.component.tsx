461cfe8c9190c3b4b761c20a3f6d1d3189f51407
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-07-08 05:37:07 +0200

    Update Layout

--- Free/2 Junior/contact-form-main/contact-form/src/components/Layout/Layout.component.tsx
+++ Free/2 Junior/contact-form-main/contact-form/src/components/Layout/Layout.component.tsx
@@ -1,5 +1,10 @@
 import { LayoutStyled } from "./Layout.styled";
+import React, { ReactNode } from "react";
 
-export const Layout = () => {
-  return <LayoutStyled>Contact form</LayoutStyled>;
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return <LayoutStyled> {children}</LayoutStyled>;
 };
