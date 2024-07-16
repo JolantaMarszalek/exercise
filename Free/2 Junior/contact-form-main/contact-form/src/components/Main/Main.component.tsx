2b7636505e5d8c696e91cdc21227b5490a527c3c
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-07-16 05:08:18 +0200

    Added lines in Main

--- Free/2 Junior/contact-form-main/contact-form/src/components/Main/Main.component.tsx
+++ Free/2 Junior/contact-form-main/contact-form/src/components/Main/Main.component.tsx
@@ -1,10 +1,20 @@
-import { MainCard, MainCardTitle, MainStyled } from "./Main.styled";
+import {
+  MainCard,
+  MainCardLineNarrow,
+  MainCardLineWide,
+  MainCardTitle,
+  MainStyled,
+} from "./Main.styled";
 
 export const Main = () => {
   return (
     <MainStyled>
       <MainCard>
         <MainCardTitle>Contact us</MainCardTitle>
+        <MainCardLineNarrow></MainCardLineNarrow>
+        <MainCardLineNarrow></MainCardLineNarrow>
+        <MainCardLineNarrow></MainCardLineNarrow>
+        <MainCardLineWide></MainCardLineWide>
       </MainCard>
     </MainStyled>
   );
